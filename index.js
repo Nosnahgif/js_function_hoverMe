2fd459de346e5fa6b4d75ca86b0cfe8b53f4edb2
Hanson Nguyen <63284611+Nosnahgif@users.noreply.github.com>
2023-01-07 00:32:16 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function swapPic() {
+    document.getElementById("picSwap").src="images/happyPup.jpeg";
+}
+function swapPic1() {
+    document.getElementById("picSwap").src="images/sadPup.jpeg";
+}
